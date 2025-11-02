0ad3c677a9d666077d67a4e1ef773aaa33c4455e
FinixGameDev <73645385+FinixGameDev@users.noreply.github.com>
2025-11-02 16:37:22 +0000

    Initial commit

---
+++ scripts/question_resource.gd
@@ -0,0 +1,12 @@
+extends Resource
+class_name Question
+
+var question : String
+var image : Texture2D
+
+var answerA : String
+var answerB : String
+var answerC : String
+var answerD : String
+
+var correctIndex : int
---
+++ scripts/quiz_resource.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name Quiz
+
+var banner : Texture2D
+var names : Array[String]
+var questions : Array[Question]
